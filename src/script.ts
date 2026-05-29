deb9e554b1c4b55cf83cbb5d50189f1107a33b79
Szymon Paluch <12szymon.paluch34@gmail.com>
2026-05-29 18:31:37 +0200

    feat: sort the array

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,10 @@
-import type { Segment } from "./types.ts";
+import type { Block, Segment } from "./types.ts";
 
 export function findThermalBlocks(segments: Segment[]) {
+    const blocks: Block[] = [];
 
+    const sortedSegments = segments.sort((prev, next) => prev.y - next.y)
+    console.log(sortedSegments);
+
+    return blocks;
 }
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,5 @@ type Rail = { x1: number; x2: number; y: number; type: 'rail' };
 type OtherRailElement = { x: number; y: number; type: 'other' };
 
 export type Segment = Rail | OtherRailElement;
+
+export type Block = { x: number; y: number; };
