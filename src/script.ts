9d4e237dbe20a5679bfcce6002c010759ad5058d
Szymon Paluch <12szymon.paluch34@gmail.com>
2026-05-29 22:33:21 +0200

    chore: rename variables to improve clarity

--- src/script.ts
+++ src/script.ts
@@ -3,14 +3,14 @@ import {scaleToBigInt} from "./helpers/bigint-conversion.ts";
 import {compareSegments} from "./helpers/sort.ts";
 
 export function findThermalBlocks(segments: Segment[]) {
-    const filteredSegments = segments.filter(segment => segment.type === "rail");
-    const sortedSegments = filteredSegments.sort(compareSegments)
+    const rails = segments.filter(segment => segment.type === "rail");
+    const sortedRails = rails.sort(compareSegments)
     const blocks: Block[] = [];
 
     let prev: Rail | undefined;
     let consecutiveLength = 0n;
-    for (let i = 0; i < sortedSegments.length - 1; i++) {
-        const current = sortedSegments[i];
+    for (let i = 0; i < sortedRails.length - 1; i++) {
+        const current = sortedRails[i];
         const difference = scaleToBigInt(current.x2) - scaleToBigInt(current.x1);
         if (prev === undefined || current.y !== prev.y || current.x1 !== prev.x2) {
             prev = current;
