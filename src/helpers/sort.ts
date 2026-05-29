4b498743fd4310217c375ddbbcdc3e86c6a05972
Szymon Paluch <12szymon.paluch34@gmail.com>
2026-05-29 21:36:48 +0200

    fix: cleanup the sorting function

--- src/helpers/sort.ts
+++ src/helpers/sort.ts
@@ -1,12 +1,12 @@
-import type {Block, Rail, Segment} from "../types.ts";
-import {scaleToBigInt, scaleToFloat} from "./bigint-conversion.ts";
+import type {Segment} from "../types.ts";
+import {scaleToBigInt} from "./bigint-conversion.ts";
 
-const ascending = (a: number, b: number) => scaleToBigInt(a) - scaleToBigInt(b)
+function ascending(a: number, b: number){
+    const bigintDiff = scaleToBigInt(a) - scaleToBigInt(b);
+    return bigintDiff > 0 ? 1 : bigintDiff < 0 ? -1 : 0;
+}
 
 export function compareSegments(a: Segment, b: Segment) {
-    const result = a.y === b.y
-        ? ascending(a.type === "rail" ? a.x1 : a.x, b.type === "rail" ? b.x1 : b.x)
-        : ascending(a.y, b.y)
-
-    return scaleToFloat(result)
+    if (a.y === b.y && a.type === "rail" && b.type === "rail") return ascending(a.x1, b.x1)
+    return ascending(a.y, b.y)
 }
